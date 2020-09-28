7dc0749a81c637bfdc67be3f7146664458a63208
Mutiara22Hasibuan <71279256+Mutiara22Hasibuan@users.noreply.github.com>
2020-09-28 23:11:54 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,1 +1,17 @@
 
+<?php
+$bilangan1 = $_GET['BILANGAN1'];
+$bilangan2 = $_GET['BILANGAN2'];
+$operasi = $_GET['operasi'];
+echo "Bilangan 1 yang dikirimkan adalah $bilangan1 <br>";
+echo "Bilangan 2 yang dikirimkan adalah $bilangan2 <br>";
+ 
+ $bil1 = $_GET['bilangan1']; $bilangan2 = $_GET['bilangan2'];
+$operasi  = $_GET['operasi']; 
+echo '<output>'; if ($operasi == "TAMBAH") 
+$hasil = $bilangan1 + $bilangan2; else if ($operasi == "KURANG") 
+$hasil = $bilangan1 - $bilangan2; else if ($operasi == "KALI") 
+$hasil = $bilangan1 * $bilangan2; else if ($operasi == "BAGI")   
+$hasil = $bilangan1 / $bilangan2;      
+ echo "Hasil perhitungannya adalah : ". $hasil; echo '</output>';
+?>
